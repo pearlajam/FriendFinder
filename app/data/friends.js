461986fbc8c52d7509dc27b53561c200b838d104
pearlajam <perlariojas@yahoo.com>
2018-10-06 02:53:45 -0600

    friend

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends =
+    [
+        {
+            "name": "Catalina",
+            "photo": "",
+            "scores": [
+               
+            ]
+        },
+        {
+            "name": "Michael",
+            "photo": "",
+            "scores": [
+              
+            ]
+        },
+        {
+            "name": "Tamitra",
+            "photo": "",
+            "scores": [
+               
+            ]
+        },
+        {
+            "name": "Kate",
+            "photo": "https://images.pexels.com/photos/814052/pexels-photo-814052.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "scores": [
+               
+            ]
+        },
+    ]
+
+module.exports = friends;
